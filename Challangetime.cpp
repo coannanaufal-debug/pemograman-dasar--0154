4858e83fab649ef500b9aeee149eec94d7606154
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-02 16:01:01 +0700

    membuat float hitungluas

---
+++ Challangetime.cpp
@@ -0,0 +1,18 @@
+//library
+#include <iostream>
+using namespace std; 
+
+//deklarasi variabel global
+float phi = 3.14159;
+int r;
+
+//impelementasi fungsi dan prosedur
+void input(){ 
+    cout << "masukan jari jari :";
+    cin >> r;
+
+}
+float hitungluas(int r){
+    return phi * r * r;
+}
+
