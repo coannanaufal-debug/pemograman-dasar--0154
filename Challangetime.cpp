8b188e799f8aa5b6b7d9741a0b8afbe719c00173
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-02 16:12:35 +0700

    membuat deklarasi variabel global

--- Challangetime.cpp
+++ Challangetime.cpp
@@ -6,20 +6,3 @@ using namespace std;
 float phi = 3.14159;
 int r;
 
-//impelementasi fungsi dan prosedur
-void input(){ 
-    cout << "masukan jari jari :";
-    cin >> r;
-
-}
-float hitungluas(int r){
-    return phi * r * r;
-}
-
-void output(){
-    cout << "hasilnya : " << hitungluas(r) << endl;
-}
-int main(){ 
-    input();
-    output();
-}
